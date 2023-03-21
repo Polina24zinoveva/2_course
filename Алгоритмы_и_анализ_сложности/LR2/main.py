ac7bef90bed694b0fb623ea7f0ce9baad85ceb75
Polina <125902170+Polina24zinoveva@users.noreply.github.com>
2023-03-21 19:34:14 +0400

    Add files via upload

---
+++ Алгоритмы_и_анализ_сложности/LR2/main.py
@@ -0,0 +1,8 @@
+def function(a, b):
+    return (a + b + abs(a - b)) // 2
+
+print("Лабораторная работа №2, 3 вариант. Выполнила Зиновьева Полина, 6203-020302D")
+print(function(5, 2))
+print(function(1, 9))
+print(function(-8, 15))
+print(function(-2, -3))
